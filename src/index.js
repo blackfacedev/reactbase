46f868f2cbd51388100740f67371b359377be4a2
Marcel Assis <marcel@marcelassis.com>
2018-07-19 11:10:18 -0300

    Ajeitando projeto base

--- src/index.js
+++ src/index.js
@@ -1,49 +1,15 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import FeedHeader from './FeedHeader';
-import Post from './Post';
 import './style.scss';
 
 class App extends Component {
-  state = {
-    posts: [
-      {
-        id: '0',
-        author: 'Você 0',
-        date: '0',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus rhoncus elementum. Nulla ligula orci, iaculis in lectus vitae, finibus tempus nunc. Etiam eu nisi tincidunt, fringilla enim nec, auctor neque. Donec dapibus nulla vitae est efficitur laoreet. Mauris in leo felis. Vestibulum vestibulum erat id mollis dapibus. Aliquam tempus efficitur nunc, vel laoreet ex lobortis lobortis. Donec imperdiet libero in dui interdum aliquam.',
-      },
-      {
-        id: '1',
-        author: 'Você 1',
-        date: '0',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus rhoncus elementum. Nulla ligula orci, iaculis in lectus vitae, finibus tempus nunc. Etiam eu nisi tincidunt, fringilla enim nec, auctor neque. Donec dapibus nulla vitae est efficitur laoreet. Mauris in leo felis. Vestibulum vestibulum erat id mollis dapibus. Aliquam tempus efficitur nunc, vel laoreet ex lobortis lobortis. Donec imperdiet libero in dui interdum aliquam.',
-      },
-      {
-        id: '2',
-        author: 'Você 2',
-        date: '0',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus rhoncus elementum. Nulla ligula orci, iaculis in lectus vitae, finibus tempus nunc. Etiam eu nisi tincidunt, fringilla enim nec, auctor neque. Donec dapibus nulla vitae est efficitur laoreet. Mauris in leo felis. Vestibulum vestibulum erat id mollis dapibus. Aliquam tempus efficitur nunc, vel laoreet ex lobortis lobortis. Donec imperdiet libero in dui interdum aliquam.',
-      },
-      {
-        id: '3',
-        author: 'Você 3',
-        date: '0',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus rhoncus elementum. Nulla ligula orci, iaculis in lectus vitae, finibus tempus nunc. Etiam eu nisi tincidunt, fringilla enim nec, auctor neque. Donec dapibus nulla vitae est efficitur laoreet. Mauris in leo felis. Vestibulum vestibulum erat id mollis dapibus. Aliquam tempus efficitur nunc, vel laoreet ex lobortis lobortis. Donec imperdiet libero in dui interdum aliquam.',
-      },
-    ],
-  };
+  state = {};
 
   render() {
     const { posts } = this.state;
     return (
       <div>
-        <FeedHeader />
-        {posts.map(post => <Post data={post} key={post.id} />)}
+test
       </div>
     );
   }
